f7288581ac7600ed423c4caef8e1e1c498c541b8
Adam Gent <94gent@gmail.com>
2024-01-02 12:32:18 -0800

    order by name edit wholesale

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,6 +1,6 @@
 import { sql } from '@vercel/postgres';
 
 export default async function getProducts(req, res) {
-  const { rows: products } = await sql`SELECT * FROM Products2 ORDER BY id ASC`;
+  const { rows: products } = await sql`SELECT * FROM Products2 ORDER BY name ASC`;
   res.status(200).json(products);
 }
