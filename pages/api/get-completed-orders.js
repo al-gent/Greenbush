fa1f75193e8f511cf617599c918a0ba349d579cb
Adam Gent <94gent@gmail.com>
2024-01-16 11:01:10 -0800

    view completed orders ?

--- pages/api/get-completed-orders.js
+++ pages/api/get-completed-orders.js
@@ -1,7 +1,7 @@
 import { sql } from '@vercel/postgres';
 
 export default async function getOrders(req, res) {
-  const orders = (await sql`SELECT * FROM Orders WHERE status != 'completed';`)
+  const orders = (await sql`SELECT * FROM Orders WHERE status = 'completed';`)
     .rows;
   res.status(200).json(orders);
 }
