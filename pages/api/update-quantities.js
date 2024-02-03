41cf60d1c024f89c60c5dd038676c33da91b4c1c
Adam Gent <94gent@gmail.com>
2024-02-03 11:38:21 -0800

    home page edits

--- pages/api/update-quantities.js
+++ pages/api/update-quantities.js
@@ -3,17 +3,17 @@ import { sql } from '@vercel/postgres';
 export default async function updateProduct(req, res) {
   const products = req.body;
   for (const product of products) {
+    const roundedQuantity = Math.round(product.quantity);
+
     await sql`
     UPDATE Products2
-    SET quantity = ${product.quantity}
+    SET quantity = ${roundedQuantity}
     WHERE id = ${product.id};
   `;
-    res
-      .status(200)
-      .json({
-        quantityUpdated: true,
-        name: product.name,
-        quantity: product.quantity,
-      });
+    res.status(200).json({
+      quantityUpdated: true,
+      name: product.name,
+      quantity: roundedQuantity,
+    });
   }
 }
