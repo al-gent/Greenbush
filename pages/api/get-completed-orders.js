25b0fdad54890603627d6ad4c7cef3daa60c3a74
Adam Gent <94gent@gmail.com>
2024-02-28 14:17:03 -0800

    completed orders are ordered by date

--- pages/api/get-completed-orders.js
+++ pages/api/get-completed-orders.js
@@ -1,7 +1,8 @@
 import { sql } from '@vercel/postgres';
 
 export default async function getOrders(req, res) {
-  const orders = (await sql`SELECT * FROM Orders WHERE status = 'completed';`)
-    .rows;
+  const orders = (
+    await sql`SELECT * FROM Orders WHERE status = 'completed' ORDER BY date DESC;`
+  ).rows;
   res.status(200).json(orders);
 }
