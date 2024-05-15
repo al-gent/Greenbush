1ccb00ac21c288e6fb0cf1398795ad1c074aba59
Adam Gent <94gent@gmail.com>
2024-05-15 13:49:08 -0700

    cookies, signup page bug fixes dashboard explanations edit wholesale changes

--- components/dash.js
+++ components/dash.js
@@ -6,6 +6,7 @@ import ReviewOrders from '../components/review-orders';
 import CompletedOrders from './completed-orders';
 import AllNewItems from './all-new-items';
 import FarmerInfo from './farmerinfo';
+
 // import AnalyzeSales from './analytics';
 
 export default function Dash({ client, isLoading, setIsLoading }) {
@@ -13,31 +14,32 @@ export default function Dash({ client, isLoading, setIsLoading }) {
   const [viewNewOrders, setViewNewOrders] = useState(false);
   const [viewAllItems, setViewAllItems] = useState(false);
   const [viewCompleted, setViewCompleted] = useState(false);
+  const [newOrderCount, setNewOrderCount] = useState('');
+
   // const [viewAnalytics, setViewAnalytics] = useState(false);
+  useEffect(() => {
+    const url = `/api/count-new-orders?client=${encodeURIComponent(client)}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setNewOrderCount(data);
+      })
+      .catch((error) => console.error('Error:', error));
+  }, [viewNewOrders, viewCompleted]);
+
   return (
     <div>
       <FarmerInfo client={client} setIsLoading={setIsLoading} />
       <div className={styles.dash}>
         <button onClick={() => setViewNewOrders(!viewNewOrders)}>
-          {viewNewOrders ? `Hide New Orders` : `New Orders`}
+          {viewNewOrders
+            ? `Hide New Orders`
+            : `${newOrderCount} New Order${newOrderCount == 1 ? ' ' : 's'}`}
         </button>
         <button onClick={() => setViewEditWholesale(!viewEditWholesale)}>
           {viewEditWholesale ? `Hide Edit Wholesale` : `Edit Wholesale`}
         </button>
       </div>
-      <div className={styles.dash}>
-        <button onClick={() => setViewCompleted(!viewCompleted)}>
-          {viewCompleted ? `Hide Completed Orders` : `Completed Orders`}
-        </button>
-        <button onClick={() => setViewAllItems(!viewAllItems)}>
-          {viewAllItems ? `Hide Harvest List` : `Harvest List`}
-        </button>
-      </div>
-      {/* <div className={styles.dash}>
-        <button onClick={() => setViewAnalytics(!viewAnalytics)}>
-          {viewAnalytics ? `Hide Analytics` : `Analytics`}
-        </button>
-      </div> */}
       {viewNewOrders && (
         <ReviewOrders
           client={client}
@@ -45,18 +47,31 @@ export default function Dash({ client, isLoading, setIsLoading }) {
           setIsLoading={setIsLoading}
         />
       )}
-      {viewAllItems && (
-        <AllNewItems client={client} setIsLoading={setIsLoading} />
-      )}
-      {viewCompleted && (
-        <CompletedOrders client={client} setIsLoading={setIsLoading} />
-      )}
       {viewEditWholesale && (
         <EditWholesale
           client={client}
           isLoading={isLoading}
           setIsLoading={setIsLoading}
         />
+      )}
+      <div className={styles.dash}>
+        <button onClick={() => setViewCompleted(!viewCompleted)}>
+          {viewCompleted ? `Hide Completed Orders` : `Completed Orders`}
+        </button>
+        <button onClick={() => setViewAllItems(!viewAllItems)}>
+          {viewAllItems ? `Hide Today's Harvest` : `Today's Harvest`}
+        </button>
+      </div>
+      {/* <div className={styles.dash}>
+        <button onClick={() => setViewAnalytics(!viewAnalytics)}>
+          {viewAnalytics ? `Hide Analytics` : `Analytics`}
+        </button>
+      </div> */}
+      {viewAllItems && (
+        <AllNewItems client={client} setIsLoading={setIsLoading} />
+      )}
+      {viewCompleted && (
+        <CompletedOrders client={client} setIsLoading={setIsLoading} />
       )}
       {/* {viewAnalytics && <AnalyzeSales client={client} />} */}
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { Analytics } from '@vercel/analytics/react';
 import '../styles/global.css';
+import { CookiesProvider } from 'react-cookie';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Analytics />
-      <Component {...pageProps} />
+      <CookiesProvider>
+        <Analytics />
+        <Component {...pageProps} />
+      </CookiesProvider>
     </>
   );
 }
