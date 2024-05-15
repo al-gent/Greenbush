437cae3c03589c487b7ca679b6a8c9046bcba905
Adam Gent <94gent@gmail.com>
2024-05-15 13:49:08 -0700

    cookies, signup page bug fixes dashboard explanations edit wholesale changes

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,36 +1,51 @@
 import Dash from '../components/dash';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
 import Login from '../components/login';
+import { parseCookies } from '../helpers';
+import DashboardHelp from '../components/dashboard-help';
 
-export default function Dashboard() {
+export async function getServerSideProps(context) {
+  const cookies = parseCookies(context.req);
+  return { props: { cookies } };
+}
+
+export default function Dashboard(cookies) {
   const [farmCode, setFarmCode] = useState('');
   const [pin, setPin] = useState('');
   const [isCred, setIsCred] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    console.log('useEffect hook', cookies);
+    if (cookies.cookies.user) {
+      setFarmCode(JSON.parse(cookies.cookies.user).farmcode);
+      setIsCred(true);
+    }
+  }, []);
+
   return (
     <div>
-      {isCred ? (
+      {isCred && (
         <div>
           <Dash
             client={farmCode}
             isLoading={isLoading}
             setIsLoading={setIsLoading}
           />
-        </div>
-      ) : (
-        <div>
-          <h1>Farmer's Dashboard</h1>
-          <Login
-            farmCode={farmCode}
-            setFarmCode={setFarmCode}
-            pin={pin}
-            setPin={setPin}
-            isCred={isCred}
-            setIsCred={setIsCred}
-          />
+          <DashboardHelp />
         </div>
       )}
+      <div>
+        <Login
+          farmCode={farmCode}
+          setFarmCode={setFarmCode}
+          pin={pin}
+          setPin={setPin}
+          isCred={isCred}
+          setIsCred={setIsCred}
+        />
+      </div>
     </div>
   );
 }
--- pages/demodemo.js
+++ pages/demodemo.js
@@ -6,6 +6,8 @@ import OrderForm from '../components/orderForm';
 import { useState } from 'react';
 import Layout from '../components/Layout';
 import Dash from '../components/dash';
+import DashboardHelp from '../components/dashboard-help';
+
 export default function Demo() {
   const [isLoading, setIsLoading] = useState(false);
 
@@ -15,7 +17,7 @@ export default function Demo() {
         <title>Farmhand Demo</title>
       </Head>
       <div className={styles.imAnApp}>
-        <h1> Hi! I'm an app. </h1>
+        <h1> Hi! I'm a free app. </h1>
         <h2>I help small farms* sell wholesale produce.</h2>
         <div>
           <div>
@@ -31,29 +33,20 @@ export default function Demo() {
               Aggregate produce from all orders into one list, so you know what
               to harvest.
             </ul>
+            <ul>Create graphs & tables to show buying trends.</ul>
             <ul>
               Make life easier for hardworking farmers by automating boring
               tasks.{' '}
             </ul>
-          </div>
-          <div>
-            <h3>I'd like to be able to: </h3>
-            <ul>
-              Predict what veggies will be listed for sale at a given time of
-              the season.
-            </ul>
-            <ul>Create pretty graphs that buying show trends.</ul>
-            <ul>Accept payments securely through the platform.</ul>
-            <ul>
-              Offer the option for buyers to create accounts and track their
-              orders.
-            </ul>
             <ul style={{ fontWeight: 'bold' }}>
-              Work for your farm too!{' '}
-              <Link href="mailto:94gent@gmail.com?subject=Hook me up with that sweet sweet app">
-                <button style={{ margin: '1rem' }}>Email Adam</button>
-              </Link>
+              Volunteer for your farm too!{' '}
             </ul>
+            <a href="mailto:94gent@gmail.com?subject=Hook me up with that sweet sweet app">
+              <button>Email Adam</button>
+            </a>
+            <Link href="./signup">
+              <button>Farmer Signup</button>
+            </Link>
           </div>
         </div>
         <p style={{ fontSize: '10px' }}>
@@ -62,15 +55,13 @@ export default function Demo() {
       </div>
 
       <div className={styles.orderFormComments}>
-        <h2>Clients order veggies here.</h2>
-        <h3>Cool features:</h3>
+        <h2>This is what a buyer sees - make an order & see how it works!</h2>
         <ul>When a purchase is made, your inventory updates automatically.</ul>
         <ul>
           Some products, like ground cherries, can be bought by the pound, or by
           the pint.
         </ul>
         <ul>When an order is submitted, an email goes to the farm.</ul>
-        <h2>Play around - make an order & see how it works!</h2>
       </div>
       <div className={styles.customerInterface}>
         <div>
@@ -82,9 +73,9 @@ export default function Demo() {
           />
         </div>
       </div>
-      <div className={styles.dashComments}>
+      {/* <div className={styles.dashComments}>
         <h2>Farmer's Dashboard Features</h2>
-        {/* <div className={styles.spreadPoints}> */}
+        <div className={styles.spreadPoints}>
         <div>
           <h3>New Orders</h3>
           <ul>View all pending orders awaiting completion.</ul>
@@ -115,18 +106,19 @@ export default function Demo() {
             <h3>Completed Orders</h3>
             <ul>View all completed orders</ul>
             <ul>Change order status if necessary</ul>
-            <h3>List of All Items</h3>
+            <h3>Harvest List</h3>
             <ul>
-              Items from all new orders are aggregated here for easy planning of
-              harvesting.
+              Items from all new orders are aggregated here for easy harvesting.
             </ul>
           </div>
         </div>
-        {/* </div> */}
-      </div>
+        </div>
+      </div> */}
 
-      <div className={styles.farmersDash}>
+      <h2>This is what the farmer sees. Check it out!</h2>
+      <div className={styles.imAnApp}>
         <Dash client="demo" isLoading={isLoading} setIsLoading={setIsLoading} />
+        <DashboardHelp />
       </div>
     </div>
   );
